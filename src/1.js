11daae311857c342cd15757af8accb19df8625bd
brainwarm <brainwor@gmail.com>
2018-01-30 14:18:54 +0200

    integrate jquery into node

--- src/1.js
+++ src/1.js
@@ -1,9 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var $ = require('jQuery');
+
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM('html/1.html');
+var $ = require('jquery')(window);
+var html = require('html');
+
+$("h1").remove();
 
 app.get('/', function(req, res) {
-    res.sendfile('src/html/1.html');
+    var title = $(html).find('title').text();
+    console.log(title);
+
+    res.sendfile('html/1.html');
 });
 
 http.listen(3000, function() {
